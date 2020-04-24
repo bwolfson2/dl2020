aa40c651f3e266387266ec9621e744a7200cc669
root <wolfson.benz@gmail.com>
2020-04-24 01:33:01 -0400

    started working on triangle dataset

---
+++ data_helper_triangle.py
@@ -0,0 +1,90 @@
+# %load data_helper.py
+import os
+from PIL import Image
+
+import numpy as np
+import pandas as pd
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+from helper import convert_map_to_lane_map, convert_map_to_road_map
+
+NUM_SAMPLE_PER_SCENE = 126
+NUM_IMAGE_PER_SAMPLE = 6
+image_names = [
+    'CAM_FRONT_LEFT.jpeg',
+    'CAM_FRONT.jpeg',
+    'CAM_FRONT_RIGHT.jpeg',
+    'CAM_BACK_LEFT.jpeg',
+    'CAM_BACK.jpeg',
+    'CAM_BACK_RIGHT.jpeg',
+    ]
+
+#BASICALLY YOU JUST HAVE TO SPECIFY THE IMAGE TO LPAD IN THE __GET_ITEM__ on init
+# The dataset class for labeled data.
+class TriangleLabeledDataset(torch.utils.data.Dataset):    
+    def __init__(self, image_folder, annotation_file, scene_index, transform,extra_info=True):
+        """
+        Args:
+            image_folder (string): the location of the image folder
+            annotation_file (string): the location of the annotations
+            scene_index (list): a list of scene indices for the unlabeled data 
+            transform (Transform): The function to process the image
+            extra_info (Boolean): whether you want the extra information
+        """
+        assert(camera in image_names)
+        self.image_folder = image_folder
+        self.annotation_dataframe = pd.read_csv(annotation_file)
+        self.scene_index = scene_index
+        self.transform = transform
+        self.extra_info = extra_info
+    
+    def __len__(self):
+        return self.scene_index.size * NUM_SAMPLE_PER_SCENE
+
+    def __getitem__(self, index):
+        if isinstance(index,tuple):
+            index,camera = index
+            assert(camera in image_names)
+            camera_index = True
+        scene_id = self.scene_index[index // NUM_SAMPLE_PER_SCENE]
+        sample_id = index % NUM_SAMPLE_PER_SCENE
+        sample_path = os.path.join(self.image_folder, f'scene_{scene_id}', f'sample_{sample_id}') 
+        
+        #get camera and only get that image
+        image_path = os.path.join(sample_path, camera)
+        image = Image.open(image_path)
+        image = self.transform(image)
+
+        data_entries = self.annotation_dataframe[(self.annotation_dataframe['scene'] == scene_id) & (self.annotation_dataframe['sample'] == sample_id)]
+        corners = data_entries[['fl_x', 'fr_x', 'bl_x', 'br_x', 'fl_y', 'fr_y','bl_y', 'br_y']].to_numpy()
+        categories = data_entries.category_id.to_numpy()
+        
+        ego_path = os.path.join(sample_path, 'ego.png')
+        ego_image = Image.open(ego_path)
+        ego_image = torchvision.transforms.functional.to_tensor(ego_image)
+        road_image = convert_map_to_road_map(ego_image)
+        
+        target = {}
+        target['bounding_box'] = torch.as_tensor(corners).view(-1, 2, 4)
+        target['category'] = torch.as_tensor(categories)
+
+        if self.extra_info:
+            actions = data_entries.action_id.to_numpy()
+            # You can change the binary_lane to False to get a lane with 
+            lane_image = convert_map_to_lane_map(ego_image, binary_lane=True)
+            
+            extra = {}
+            extra['action'] = torch.as_tensor(actions)
+            extra['ego_image'] = ego_image
+            extra['lane_image'] = lane_image
+
+            return image, target, road_image, extra
+        
+        else:
+            return image, target, road_image
+
+    

d9c0cbb682f42f1f911b72e60cc9719dd5812129
Ben Wolfson <wolfson.benz@gmail.com>
2020-04-26 02:54:01 -0400

    cahnged the google bucket to client secrets

--- bucket_upload.sh
+++ bucket_upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcloud auth activate-service-account  --key-file=sinuous-client-274919-bb1d7df46d87.json
+gcloud auth activate-service-account  --key-file=client_secrets.json
 for var in "$@"
 do
     gsutil cp "$var" gs://dl2020
